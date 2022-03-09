bcdd67b7d50a14171a6770dbcd7bce598cde4ecd
Barry Dwyer <dwyerbaz@gmail.com>
2022-03-09 23:20:23 +0200

    Needed a bin

--- goinstant/buildreleases.sh
+++ goinstant/buildreleases.sh
@@ -1,5 +1,6 @@
 version=$1
 echo $version
+mkdir -p bin
 GOOS=darwin GOARCH=amd64 go build -ldflags="-X 'goinstant.BuildVersion=$version'" && mv ./goinstant ./bin/goinstant-macos \
 && GOOS=linux GOARCH=amd64 go build -ldflags="-X 'goinstant.BuildVersion=$version'" && mv ./goinstant ./bin/goinstant-linux \
 && GOOS=windows GOARCH=amd64 go build -ldflags="-X 'goinstant.BuildVersion=$version'" && mv ./goinstant.exe ./bin/goinstant.exe \
